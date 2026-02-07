d13bda4a4feee9355d1c75da52ca38530db105a3
stephenbrad515 <stephenbrad515@gamil.com>
2026-02-06 22:34:28 -0500

    added variables

---
+++ variables.tf
@@ -0,0 +1,48 @@
+
+variable "project_id" {
+  description = "The GCP Project ID"
+  type        = string
+  default = "testingterraform2"
+}
+
+variable "region" {
+  description = "The region to deploy resources in"
+  type        = string
+  default     = "us-east4"
+}
+
+variable "zone" {
+  description = "The zone to deploy the VM in"
+  type        = string
+  default     = "us-east4-a"
+}
+
+variable "machine_type" {
+  description = "The size of the VM"
+  type        = string
+  default     = "e2-medium"
+}
+
+#Reference the Machine Image
+//data "google_compute_machine_image" "source_image" {
+//  name = "ubuntu-xfce"
+//}
+
+
+variable "ubuntu_image" {
+  description = "The OS image to use"
+  type        = string
+  default     = "ubuntu-os-cloud/ubuntu-2204-lts"
+}
+/*
+variable "ssh_user" {
+  description = "The username for SSH access"
+  type        = string
+}
+
+variable "public_key_path" {
+  description = "Path to the public SSH key file"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+}
+*/
